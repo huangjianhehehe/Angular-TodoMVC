e6f9004f44b4d4bb3980084be3ede8f4e2e0a1e8
huangjianhehehe <786762595@qq.com>
2020-04-30 00:04:53 +0800

    列表渲染,处理样式

--- Angular-TodoMVC/src/app/app.component.ts
+++ Angular-TodoMVC/src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+const todos = [
+    {
+      id:1,
+      title:'吃饭',
+      done:true
+    },
+    {
+      id:2,
+      title:'唱歌',
+      done:false
+    },
+     {
+      id:3,
+      title:'写代码',
+      done:true
+    }
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// 导入列表数据
 export class AppComponent {
-  title = 'Angular-TodoMVC';
+  title = 'angular';
+  public todos:{
+    id:number,
+    title:string,
+    done:boolean
+  }[] =todos
+
+  addTodo (e):void{
+    const titleText = e.target.value
+    if (!titleText.length){
+      return
+    }
+    const last = this.todos[todos.length-1]
+    this.todos.push({
+        id:last?last.id+1:1,
+        title:titleText,
+        done:false
+    })
+    e.target.value=""
+    console.log(this.todos)
+  }
 }
